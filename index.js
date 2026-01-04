eeb90bdaf1c2327dcb9ad78c2e2b62fb4674073f
Vassia Christ <christvassia@gmail.com>
2026-01-04 16:50:08 +0100

    dirfile deployment error fixed

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ import sharp from "sharp";
 import { fileURLToPath } from "url";
 
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 /* ------------------ Vercel Base64 Key Setup ------------------ */
 if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
@@ -36,8 +39,7 @@ console.log("LOCATION:", process.env.LOCATION);
 console.log("PROCESSOR_ID:", process.env.PROCESSOR_ID);
 
 /* ------------------ Setup ------------------ */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
